b981bd0863bba331cfcc01ec426863c4846e1041
Niru1pam2 <nirupamtalukdar123@gmail.com>
2024-11-17 19:50:54 +0530

    deploy

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
+const path = require("path");
 const express = require("express");
 require("dotenv").config();
+const cookieParser = require("cookie-parser");
 
 //routes imports
 const authRoutes = require("./routes/authRoutes");
@@ -7,9 +9,9 @@ const messageRoutes = require("./routes/messageRoutes");
 const userRoutes = require("./routes/userRoutes");
 
 const connectDB = require("./db/connect");
-const PORT = process.env.PORT || 5000;
-const cookieParser = require("cookie-parser");
 const { app, server } = require("./socket/socket");
+
+const PORT = process.env.PORT || 5000;
 
 //middlewares
 app.use(express.json());
@@ -17,6 +19,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join("frontend", "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join("frontend", "dist", "index.html"));
+});
 
 const start = async () => {
   try {
